424f7056bdb79c58212bf8350623fc437545adcb
Shirfeng Chang <shirfeng.chang@gmail.com>
2017-03-04 02:37:54 -0800

    chefserver setup p2

--- chefdownloads.sh
+++ chefdownloads.sh
@@ -1,4 +1,7 @@
 #/usr/bin/env bash
+# Standalone config https://docs.chef.io/install_server.html#standalone
+# chef-server-ctl user-create USER_NAME FIRST_NAME LAST_NAME EMAIL 'PASSWORD' --filename FILE_NAME
+# chef-server-ctl org-create short_name 'full_organization_name' --association_user user_name --filename ORGANIZATION-validator.pem
 chef-server-ctl install chef-manage
 chef-server-ctl reconfigure
 chef-manage-ctl reconfigure --accept-license
@@ -9,4 +12,4 @@ opscode-push-jobs-server-ctl reconfigure
 
 chef-server-ctl install opscode-reporting
 chef-server-ctl reconfigure
-opscode-reporting-ctl reconfigure
+opscode-reporting-ctl reconfigure --accept-license
